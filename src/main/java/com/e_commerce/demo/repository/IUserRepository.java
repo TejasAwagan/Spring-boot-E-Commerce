d1d73f93deb8f2733c3cfd72999dd183ae672c43
Tejas Awagan <tejas.awagan29@gmail.com>
2025-04-12 01:47:37 +0530

    user repository created

--- src/main/java/com/e_commerce/demo/repository/IUserRepository.java
+++ src/main/java/com/e_commerce/demo/repository/IUserRepository.java
@@ -3,7 +3,14 @@ package com.e_commerce.demo.repository;
 
 import com.e_commerce.demo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface IUserRepository extends JpaRepository<User,String> {
 
+    Optional<User> findByEmail(String email);
+
+    Optional<User> existsByEmail(String email);
 }
